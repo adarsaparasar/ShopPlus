b9d03b264d0a07344b0a8c864555b44bcccfd153
adarsaparasar <adarsaparasarbehera@gmail.com>
2024-07-29 20:20:30 +0530

    testing 2

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -169,7 +169,12 @@ const HomePage = () => {
               {products?.map((p) => (
                
               <div className="card m-2" style={{ width: "18rem" }} key={p._id}>
-                
+                <img
+                  src={`https://shopplus-oej3.onrender.com/api/v1/product/product-photo/${p._id}`}
+                  className="card-img-top"
+                  alt={p.name}
+                    
+                />
                 <div className="card-body">
                   <h5 className="card-title">{p.name}</h5>
                   <p className="card-text">
